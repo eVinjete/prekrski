2a09c82d6ac5612a0e7d7b342b37c8fd3c075b6b
MŠvigelj <ms0555@student.uni-lj.si>
2022-01-10 22:24:08 +0100

    Try 4.

--- src/main/java/si/evinjete/prekrski/Kamera.java
+++ src/main/java/si/evinjete/prekrski/Kamera.java
@@ -1,22 +1,14 @@
 package si.evinjete.prekrski;
 
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-@Entity
+
 public class Kamera implements Serializable {
-    @Column(nullable = false)
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     public Integer id;
-    @Column(name = "password", nullable = false, updatable = false)
     public String password;
-    @Column(name = "location", nullable = false, updatable = false)
     public String location;
-    @Column(name = "timestamp", nullable = false, updatable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     public Date timestamp;
-    @Column(name = "direction", nullable = false, updatable = false)
     public String direction;
 }
